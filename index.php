c416762df51bcd461ad0a8879f3c653cd9992c2f
Alex <62556492+alexmilner20@users.noreply.github.com>
2025-05-25 23:48:19 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>VellumCMS</title>
+</head>
+<body>
+<h1> A CMS built for people doing good — not profits. No fees. Just freedom.</h1>
+</body>
+</html>
