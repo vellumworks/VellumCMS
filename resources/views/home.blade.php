17474dcf84b0e8871efbb12cf3a4e06626fc1857
Alex <62556492+alexmilner20@users.noreply.github.com>
2025-06-03 00:41:57 +0100

    lavarvel and setup
    installing laravel and setup

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ asset('assets/styles.css') }}">
+    <title>Content Management Systems Built For Charities | VellumCMS</title>
+</head>
+<body>
+<section class="header">
+    <h1>The Open-Source CMS for Communities That Care</h1>
+    <p>VellumCMS is a purpose-built, open-source content management system designed for charities, grassroots organisations, and underserved communities. Lightweight, multilingual, and privacy-first, it empowers anyone — regardless of technical ability or resources — to build and manage ethical, mission-driven websites.</p>
+</section>
+</body>
+</html>
